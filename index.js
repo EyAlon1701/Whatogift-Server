b6549baebfd3a8ca04088b9f1cbe0013c1eec5ce
EyAlon1701 <eyalon022@gmail.com>
2022-10-25 11:27:32 +0300

    25102022

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bp from 'body-parser';
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bp.urlencoded({extended:false}))
+app.use(bp.json())
+
+const mongoUrl = 'mongodb+srv://whatogift-user:bbIqlPaVcqEijwsP@cluster0.zwkaydg.mongodb.net/wahtogift?retryWrites=true&w=majority';
+
+///////////////////////////////ROUTES/////////////////////////////////
+import accountRoute from './controllers/account.js';
+app.use('/api/account', accountRoute);
+//---------------------END OF ROUTES -----------------------------////
+
+const port = 3001;
+
+mongoose.connect(mongoUrl)
+.then(results => {
+    console.log(results);
+    app.listen(port, function(){
+        console.log(`Server is running via port ${port}`);
+    });
+})
+.catch(error => { console.log(error.message) })
---
+++ models/account.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    firstName: String,
+    lastName: String,
+    dob: Date,
+    gender: String,
+    avatar: { type: String, default: 'https://w1.pngwing.com/pngs/509/295/png-transparent-circle-design-avatar-cartoon-football-video-games-color-sports-equipment-world.png'},
+    isVerified: {type: Boolean, default:false},
+    passcode: Number,
+    contact: {
+        address: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        mobile: String,
+    }
+})
+
+export default mongoose.model('Account', accountSchema);
